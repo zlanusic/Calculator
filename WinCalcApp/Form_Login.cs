76255153533cd2e3a651238b92c90f730c7199a9
ZlatkoAnusic <zlatko.anusic@zlanusic.com>
2013-06-22 12:19:57 +0200

    WinCalcApp

---
+++ WinCalcApp/Form_Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WinCalcApp
+{
+    public class LoginForm : Form
+    {
+        public static void Main(string[] args)
+        {
+            //basic login forma
+
+
+        }
+    }
+}
